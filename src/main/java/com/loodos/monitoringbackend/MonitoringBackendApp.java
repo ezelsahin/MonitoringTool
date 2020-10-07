d1b8170f002aa1ffecb1f10a15fd762cb0331099
ezelsahin <56742339+ezelsahin@users.noreply.github.com>
2020-10-07 14:22:48 +0300

    Add files via upload

---
+++ src/main/java/com/loodos/monitoringbackend/MonitoringBackendApp.java
@@ -0,0 +1,13 @@
+package com.loodos.monitoringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringBackendApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringBackendApp.class, args);
+	}
+
+}
