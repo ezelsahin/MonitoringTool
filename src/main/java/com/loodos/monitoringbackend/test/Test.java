481fed2ea1b9a4f07108718e1fb9ec87bdaf80bb
ezelsahin <56742339+ezelsahin@users.noreply.github.com>
2020-10-07 14:27:50 +0300

    Add files via upload

---
+++ src/main/java/com/loodos/monitoringbackend/test/Test.java
@@ -0,0 +1,13 @@
+package com.loodos.monitoringbackend.test;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test {
+    @RequestMapping("/test")
+    String test() {
+        return "test";
+    }
+}
