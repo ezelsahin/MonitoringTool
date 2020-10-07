e940523a3df6e3cf0339835fee8b5b92fd1f19cf
ezelsahin <56742339+ezelsahin@users.noreply.github.com>
2020-10-07 14:31:04 +0300

    Add files via upload

---
+++ src/test/java/com/loodos/monitoringbackend/MonitoringBackendAppTests.java
@@ -0,0 +1,13 @@
+package com.loodos.monitoringbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringBackendAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
